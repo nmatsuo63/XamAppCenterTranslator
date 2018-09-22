915efe465bc2827eea571c41eba1f48422513688
nmatsuo63 <nmatsuo.63@gmail.com>
2018-09-22 18:53:59 +0900

    First commit

---
+++ UITests/AppInitializer.cs
@@ -0,0 +1,25 @@
+﻿namespace XamAppCenterSample2018.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .PreferIdeSettings()
+                    .InstalledApp("<あなたのアプリのパッケージ名>")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .EnableLocalScreenshots()
+                .PreferIdeSettings()
+                .InstalledApp("com.nmatsuo63.XamAppCenterSample2018")
+                .StartApp();
+        }
+    }
+}
